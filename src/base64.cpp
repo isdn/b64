c4c934b4d82627ef2b6df0af10dd04d227467042
Denis <issden@gmail.com>
2022-02-06 22:03:08 +0400

    Minor refactoring (thanks to @BlackCrowned)

--- src/base64.cpp
+++ src/base64.cpp
@@ -10,64 +10,74 @@ inline bool Base64::is_base64(unsigned char c) {
 }
 
 std::string Base64::encode(const std::string &s) {
-    uint8_t i = 0, j = 0;
-    unsigned char char_array_3[3], char_array_4[4];
+    uint8_t i = 0U;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
     std::string ret;
 
     for (const char &c : s) {
         char_array_3[i++] = c;
-        if (i == 3) {
-            char_array_4[0] = (char_array_3[0] & 0xfcu) >> 2u;
-            char_array_4[1] = ((char_array_3[0] & 0x03u) << 4u) + ((char_array_3[1] & 0xf0u) >> 4u);
-            char_array_4[2] = ((char_array_3[1] & 0x0fu) << 2u) + ((char_array_3[2] & 0xc0u) >> 6u);
-            char_array_4[3] = char_array_3[2] & 0x3fu;
-            for (i=0; i<4; i++)
-                ret += b64_chars[char_array_4[i]];
-            i = 0;
+        if (i == 3U) {
+            char_array_4[0] = (char_array_3[0] & 0xfcU) >> 2U;
+            char_array_4[1] = ((char_array_3[0] & 0x03U) << 4U) + ((char_array_3[1] & 0xf0U) >> 4U);
+            char_array_4[2] = ((char_array_3[1] & 0x0fU) << 2U) + ((char_array_3[2] & 0xc0U) >> 6U);
+            char_array_4[3] = char_array_3[2] & 0x3fU;
+            for (uint8_t j=0U; j<4U; j++) {
+                ret += b64_chars[char_array_4[j]];
+            }
+            i = 0U;
         }
     }
     if (i) {
-        for (j=i; j<3; j++)
+        for (uint8_t j=i; j<3U; j++) {
             char_array_3[j] = '\0';
-        char_array_4[0] = (char_array_3[0] & 0xfcu) >> 2u;
-        char_array_4[1] = ((char_array_3[0] & 0x03u) << 4u) + ((char_array_3[1] & 0xf0u) >> 4u);
-        char_array_4[2] = ((char_array_3[1] & 0x0fu) << 2u) + ((char_array_3[2] & 0xc0u) >> 6u);
-        for (j=0; j<i+1; j++)
+        }
+        char_array_4[0] = (char_array_3[0] & 0xfcU) >> 2U;
+        char_array_4[1] = ((char_array_3[0] & 0x03U) << 4U) + ((char_array_3[1] & 0xf0U) >> 4U);
+        char_array_4[2] = ((char_array_3[1] & 0x0fU) << 2U) + ((char_array_3[2] & 0xc0U) >> 6U);
+        for (uint8_t j=0U; j<i+1U; j++) {
             ret += b64_chars[char_array_4[j]];
-        while(i++ < 3)
+        }
+        while(i++ < 3U) {
             ret += '=';
+        }
     }
     return ret;
 }
 
 std::string Base64::decode(const std::string &s) {
-    uint8_t i = 0, j = 0, k = 0;
-    unsigned char char_array_4[4], char_array_3[3];
+    uint8_t i = 0U;
+    std::size_t k = 0U;
     std::string ret;
-    size_t in_len = s.size();
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+    std::size_t in_len = s.size();
 
     while (in_len-- && (s[k] != '=') && is_base64(s[k])) {
         char_array_4[i++] = s[k];
         k++;
-        if (i == 4) {
-            for (i=0; i<4; i++)
-                char_array_4[i] = b64_chars.find(char_array_4[i]) & 0xffu;
-            char_array_3[0] = (char_array_4[0] << 2u) + ((char_array_4[1] & 0x30u) >> 4u);
-            char_array_3[1] = ((char_array_4[1] & 0xfu) << 4u) + ((char_array_4[2] & 0x3cu) >> 2u);
-            char_array_3[2] = ((char_array_4[2] & 0x3u) << 6u) + char_array_4[3];
-            for (i=0; i<3; i++)
-                ret += char_array_3[i];
-            i = 0;
+        if (i == 4U) {
+            for (uint8_t j=0U; j<4U; j++) {
+                char_array_4[j] = b64_chars.find(char_array_4[j]) & 0xffU;
+            }
+            char_array_3[0] = (char_array_4[0] << 2U) + ((char_array_4[1] & 0x30U) >> 4U);
+            char_array_3[1] = ((char_array_4[1] & 0x0fU) << 4U) + ((char_array_4[2] & 0x3cU) >> 2U);
+            char_array_3[2] = ((char_array_4[2] & 0x03U) << 6U) + char_array_4[3];
+            for (uint8_t j=0U; j<3U; j++) {
+                ret += char_array_3[j];
+            }
+            i = 0U;
         }
     }
     if (i) {
-        for (j=0; j<i; j++)
-            char_array_4[j] = b64_chars.find(char_array_4[j]) & 0xffu;
-        char_array_3[0] = (char_array_4[0] << 2u) + ((char_array_4[1] & 0x30u) >> 4u);
-        char_array_3[1] = ((char_array_4[1] & 0xfu) << 4u) + ((char_array_4[2] & 0x3cu) >> 2u);
-        for (j=0; j<i-1; j++)
+        for (uint8_t j=0U; j<i; j++) {
+            char_array_4[j] = b64_chars.find(char_array_4[j]) & 0xffU;
+        }
+        char_array_3[0] = (char_array_4[0] << 2U) + ((char_array_4[1] & 0x30U) >> 4U);
+        char_array_3[1] = ((char_array_4[1] & 0x0fU) << 4U) + ((char_array_4[2] & 0x3cU) >> 2U);
+        for (uint8_t j=0U; j<i-1U; j++) {
             ret += char_array_3[j];
+        }
     }
     return ret;
 }
-
